723dcc9aad18c25ce201466de319108b244dc65c
Briggs Elsperger <briggse@protonmail.com>
2019-08-10 23:06:46 -0500

    adds CSV parsing

--- main.cpp
+++ main.cpp
@@ -1,23 +1,49 @@
 #include <iostream>
-#include "stdio.h"
+#include <vector>
 #include "tinyxml2.h"
 #include "raylib.h"
 
+std::vector<int> parseGidCsv(std::string csv) {
+  std::vector<int> numVector;
+
+  // str.find works with positions, not indexes
+  std::size_t currentStringPos = 1;
+
+  while(currentStringPos != 0) {
+    std::size_t delimiterPos = csv.find(',', currentStringPos);
+    numVector.push_back(std::stoi(csv.substr(currentStringPos, delimiterPos)));
+    currentStringPos = delimiterPos + 1;
+  }
+
+  return numVector;
+}
+
 int main(void) {
-  tinyxml2::XMLDocument doc;
-  doc.LoadFile("assets/test.tsx");
-
-  const char* value = doc.FirstChildElement()->FirstChildElement()->Attribute("width");
-  std::cout << value << '\n';
-
-// Initialization
-    //--------------------------------------------------------------------------------------
+  // Initialization
+  //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
+    // Importing map1.tmx for csv
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile("assets/map1.tmx");
+
+    tinyxml2::XMLElement* mapElement = doc.RootElement();
+    const int mapWidth = mapElement->IntAttribute("width");   // width in tiles
+    const int mapHeight = mapElement->IntAttribute("height"); // Height in tiles
+
+    tinyxml2::XMLElement* tilesetElement = mapElement->FirstChildElement("tileset");
+    const int tileWidth = tilesetElement->IntAttribute("tileWidth");
+    const int tileHeight = tilesetElement->IntAttribute("tileHeight");
+    const int tileCount = tilesetElement->IntAttribute("tilecount");
+    const int columns = tilesetElement->IntAttribute("columns");
+
+    tinyxml2::XMLElement* dataElement = mapElement->FirstChildElement("layer")->FirstChildElement("data");
+    std::vector<int> gids = parseGidCsv(dataElement->GetText());
+
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-    // Texture2D tileset = LoadTexture("assets/TileSet.png");
+    Texture2D tileset = LoadTexture("assets/ForgottenDungeon.png");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -35,7 +61,7 @@ int main(void) {
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
-            // DrawTexturePro(tileset, {841, , 25, 25}, {0, 0, screenWidth, screenHeight}, {0, 0}, 0, WHITE);
+            DrawTexturePro(tileset, {0, 0, (float)tileset.width, (float)tileset.height}, {0, 0, screenWidth, screenHeight}, {0, 0}, 0, WHITE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
